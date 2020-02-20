358d7ffc3153dee073e17537f9a08f72ee033c9e
jcc <joel.chacon@cimat.mx>
2020-02-20 14:20:00 -0600

    ..

--- qualification/main.cpp
+++ qualification/main.cpp
@@ -6,59 +6,97 @@ vector<vector<int> > books_in_library;
 vector<int> counter_library;
 
 ///the representation is by index of libraries and a mask indicating which book is active in each library..
-//struct x_solution
-//{
-//
-//};
-vector<int> x_solution; //indexes of the libraries...
-vector< vector<int> > mask_books_library;
-vector<int> size_scanning;
-long eval()
+struct x_solution
 {
+   vector<int> x; //indexes of the libraries...
+   vector< vector<int> > B_L;
+};
 
+void print_solution(struct x_solution &sol);
+long eval(struct x_solution &Sol)
+{
+ long total_score = 0;  
+  //get the start processing of each library.....
+  vector<int> start(L);
+//  for(int i = 0; i start.size(); i++)
+//  {
+//     c_time += times_l[i]
+//     start[i] = c_time;
+//  }
+
+  int cont = 0;
+  vector<int> c_lib(Sol.x[cont]), c_book_lib(L);
+  int current_day = shipping_l[cont]+1;
+
+  while(current_day < D)
+  {
+     for(int t = 0; t < shipping_l[cont]; t++)
+     {
+	current_day++;
+        for(int l = 0; l < c_lib.size(); l++)
+        { 
+           for(int i = 0; i < times_l[c_lib[l]];i++)
+           {
+              total_score += scores_books[ books_in_library[c_lib[l]][ c_book_lib[c_lib[l]] ]];
+              c_book_lib[c_lib[l]]++;
+           }
+        }
+     }
+     //add library...
+     c_lib.push_back(Sol.x[cont++]);
+  }
+ return total_score;
 }
 
 int main()
 {
+  struct x_solution sol;
+  srand(1);
   scanf("%d %d %d\n", &B, &L, &D);
   scores_books.resize(B);
   counter_library.resize(B, 0);
   for(int i = 0; i < B; i++) scanf("%d ", &scores_books[i]);
   books_in_library.resize(L);
-  mask_books_library.resize(L); 
-  size_scanning.resize(L);
+  sol.B_L.resize(L); 
+  sol.x.resize(L);
+
   times_l.resize(L);
   shipping_l.resize(L);
   for(int i = 0; i < L; i++)
   { 
     int size_library;
     scanf("%d %d %d\n", &size_library, &times_l[i], &shipping_l[i]); 
+    //cout << size_library<< " " << times_l[i] << " " <<shipping_l[i]<<endl;
     books_in_library[i].resize(size_library);
-    mask_books_library[i].resize(size_library, 1); //all the book are active....
-    size_scanning[i] = size_library;
+    sol.B_L[i].resize(size_library, 1); //all the book are active....
     for(int j = 0; j < books_in_library[i].size(); j++)
     {
        scanf("%d ", &books_in_library[i][j]);
+       sol.B_L[i].push_back(books_in_library[i][j]);
        counter_library[books_in_library[i][j]]++;
     }
   }
 
   //generating a random solution...
-  vector<int> x_solution(L);
   for(int i = 0; i < L; i++)
-    x_solution[i] = i; 
-  random_shuffle(x_solution.begin(), x_solution.end());
-  
-  printf("%d\n", x_solution.size());
-  for(int i = 0; i < x_solution.size(); i++)
+    sol.x[i] = i; 
+  random_shuffle(sol.x.begin(), sol.x.end());
+
+  printf("%d\n\n", eval(sol));
+  print_solution(sol);
+
+  return 0;
+}
+void print_solution(struct x_solution &sol)
+{
+  printf("%d\n", sol.x.size());
+  for(int i = 0; i < sol.x.size(); i++)
   {
-     printf("%d %d\n", x_solution[i], size_scanning[x_solution[i]]);
-     for(int j = 0; j < mask_books_library[x_solution[i]].size(); j++)
+     printf("%d %d\n", sol.x[i], sol.B_L[sol.x[i]].size());
+     for(int j = 0; j < sol.B_L[sol.x[i]].size(); j++)
      {
-       if(mask_books_library[x_solution[i]][j])
-       printf("%d ",books_in_library[x_solution[i]][j]);
+       printf("%d ", sol.B_L[sol.x[i]][j]);
      }
      printf("\n");
   }
-  return 0;
 }
