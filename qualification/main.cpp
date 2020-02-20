fcd83a70faaa507656ecd1f838e65279ab0edf09
Joel <joel.chacon@cimat.mx>
2020-02-20 15:44:33 -0600

    ..

--- qualification/main.cpp
+++ qualification/main.cpp
@@ -11,40 +11,68 @@ struct x_solution
    vector<int> x; //indexes of the libraries...
    vector< vector<int> > B_L;
 };
-
+struct x_solution iterated_local_search(struct x_solution initial);
 void print_solution(struct x_solution &sol);
 long eval(struct x_solution &Sol)
 {
  long total_score = 0;  
-  //get the start processing of each library.....
-  vector<int> start(L);
-//  for(int i = 0; i start.size(); i++)
-//  {
-//     c_time += times_l[i]
-//     start[i] = c_time;
-//  }
-
-  int cont = 0;
-  vector<int> c_lib(Sol.x[cont]), c_book_lib(L);
-  int current_day = shipping_l[cont]+1;
-
-  while(current_day < D)
+  vector<int> start(L), cont_book_lib(L, 0);
+  vector<bool> checked(B, false);
+  start[0] = times_l[Sol.x[0]];
+  for(int i = 1; i < L; i++) start[i] = start[i-1]+times_l[Sol.x[i]];
+  for(int d = 0; d <D; d++)
   {
-     for(int t = 0; t < shipping_l[cont]; t++)
+     for(int l = 0; l < L; l++)
      {
-	current_day++;
-        for(int l = 0; l < c_lib.size(); l++)
-        { 
-           for(int i = 0; i < times_l[c_lib[l]];i++)
-           {
-              total_score += scores_books[ books_in_library[c_lib[l]][ c_book_lib[c_lib[l]] ]];
-              c_book_lib[c_lib[l]]++;
-           }
-        }
+	int id_lib = Sol.x[l];
+	if( d >= start[l] )
+	{
+	  for(int t = 0; t <  shipping_l[id_lib]; t++)
+	  {
+            if( cont_book_lib[id_lib] < books_in_library[id_lib].size())
+            {
+            	      int id_book = books_in_library[id_lib][cont_book_lib[id_lib]++];
+            		 if( checked[id_book]) continue;
+                             total_score += scores_books[id_book];
+            		 checked[id_book]=true;
+            }
+	  }
+	}
      }
-     //add library...
-     c_lib.push_back(Sol.x[cont++]);
   }
+//  int cont = 0;
+//  vector<int> current_lib, cont_book_lib(L,0);
+//  current_lib.push_back(Sol.x[0]);
+//  int current_day = shipping_l[0];
+//  vector<bool> checked(B, false);
+//  while(current_day < D)
+//  {
+//       cont++;
+//     for(int t = 0; t < shipping_l[cont]; t++)
+//     {
+//        for(int l = 0; l < current_lib.size(); l++)
+//        { 
+//	   int id_lib = current_lib[l];
+//           for(int i = 0; i < times_l[id_lib];i++)
+//           {
+//               if( cont_book_lib[id_lib] < books_in_library[id_lib].size())
+//               {
+//	         int id_book = books_in_library[id_lib][cont_book_lib[id_lib]++];
+//		 if( checked[id_book]) continue;
+//	         //if has not been processed....
+//                 total_score += scores_books[id_book];
+//		 checked[id_book]=true;
+//		}
+//           }
+//        }
+//	if(current_day >= D) break;
+//	current_day++;
+//	cout  << current_day<< " .. "<<D<< " ";
+//     }
+     //add library...
+//     if(cont >= Sol.x.size()) break;
+     //current_lib.push_back(Sol.x[cont]);
+  //}
  return total_score;
 }
 
@@ -68,7 +96,7 @@ int main()
     scanf("%d %d %d\n", &size_library, &times_l[i], &shipping_l[i]); 
     //cout << size_library<< " " << times_l[i] << " " <<shipping_l[i]<<endl;
     books_in_library[i].resize(size_library);
-    sol.B_L[i].resize(size_library, 1); //all the book are active....
+//    sol.B_L[i].resize(size_library, 1); //all the book are active....
     for(int j = 0; j < books_in_library[i].size(); j++)
     {
        scanf("%d ", &books_in_library[i][j]);
@@ -81,9 +109,9 @@ int main()
   for(int i = 0; i < L; i++)
     sol.x[i] = i; 
   random_shuffle(sol.x.begin(), sol.x.end());
-
-  printf("%d\n\n", eval(sol));
-  print_solution(sol);
+  struct x_solution improved = iterated_local_search(sol);
+//  printf("%d\n\n", eval(improved));
+  print_solution(improved);
 
   return 0;
 }
@@ -99,4 +127,23 @@ void print_solution(struct x_solution &sol)
      }
      printf("\n");
   }
+}
+
+struct x_solution iterated_local_search(struct x_solution best)
+{
+  int cont = 500;
+  long f_best= eval(best);
+
+  while(cont--)
+  {
+    struct x_solution current = best;
+    random_shuffle(current.x.begin(), current.x.end());
+    long f_current = eval(current);
+    if( f_current > f_best)
+    {
+	f_best= f_current;
+	best = current;
+    }
+  }
+ return best;
 }
