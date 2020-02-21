fe51e1ecc9238c43c2a38bb6b4ff514a9e89ad66
Joel <joel.chacon@cimat.mx>
2020-02-20 18:32:10 -0600

    ..

--- qualification/main.cpp
+++ qualification/main.cpp
@@ -15,19 +15,19 @@ struct x_solution iterated_local_search(struct x_solution initial);
 void print_solution(struct x_solution &sol);
 long eval(struct x_solution &Sol)
 {
- long total_score = 0;  
-  vector<int> start(L), cont_book_lib(L, 0);
+ long total_score = 0;
+  vector<int> cont_book_lib(L, 0);
   vector<bool> checked(B, false);
-  start[0] = times_l[Sol.x[0]];
-  for(int i = 1; i < L; i++) start[i] = start[i-1]+times_l[Sol.x[i]];
-  for(int d = 0; d <D; d++)
+  int cont = 0;
+  int current_time = times_l[Sol.x[cont]];
+  for(int d = current_time; d <D; d++)
   {
-     for(int l = 0; l < L; l++)
+     if(d >= current_time )
+       current_time +=times_l[Sol.x[++cont]];
+     for(int l = 0; l < cont; l++)//can be improved..
      {
-	int id_lib = Sol.x[l];
-	if( d >= start[l] )
-	{
-	  for(int t = 0; t <  shipping_l[id_lib]; t++)
+	  int id_lib = Sol.x[l];
+	  for(int t = 0; t <  shipping_l[id_lib]; t++)//required..
 	  {
             if( cont_book_lib[id_lib] < books_in_library[id_lib].size())
             {
@@ -37,42 +37,8 @@ long eval(struct x_solution &Sol)
             		 checked[id_book]=true;
             }
 	  }
-	}
      }
   }
-//  int cont = 0;
-//  vector<int> current_lib, cont_book_lib(L,0);
-//  current_lib.push_back(Sol.x[0]);
-//  int current_day = shipping_l[0];
-//  vector<bool> checked(B, false);
-//  while(current_day < D)
-//  {
-//       cont++;
-//     for(int t = 0; t < shipping_l[cont]; t++)
-//     {
-//        for(int l = 0; l < current_lib.size(); l++)
-//        { 
-//	   int id_lib = current_lib[l];
-//           for(int i = 0; i < times_l[id_lib];i++)
-//           {
-//               if( cont_book_lib[id_lib] < books_in_library[id_lib].size())
-//               {
-//	         int id_book = books_in_library[id_lib][cont_book_lib[id_lib]++];
-//		 if( checked[id_book]) continue;
-//	         //if has not been processed....
-//                 total_score += scores_books[id_book];
-//		 checked[id_book]=true;
-//		}
-//           }
-//        }
-//	if(current_day >= D) break;
-//	current_day++;
-//	cout  << current_day<< " .. "<<D<< " ";
-//     }
-     //add library...
-//     if(cont >= Sol.x.size()) break;
-     //current_lib.push_back(Sol.x[cont]);
-  //}
  return total_score;
 }
 
@@ -109,42 +75,52 @@ int main()
   for(int i = 0; i < L; i++)
     sol.x[i] = i; 
   random_shuffle(sol.x.begin(), sol.x.end());
+//  cout << eval(sol)<<endl;
+ // print_solution(sol);
   struct x_solution improved = iterated_local_search(sol);
-//  printf("%d\n\n", eval(improved));
+//  printf("%ld\n\n", eval(improved));
   print_solution(improved);
 
   return 0;
 }
 void print_solution(struct x_solution &sol)
 {
-  printf("%d\n", sol.x.size());
+  //printf("%d\n", (int)sol.x.size());
+  fprintf(stderr, "%d\n", (int)sol.x.size());
   for(int i = 0; i < sol.x.size(); i++)
   {
-     printf("%d %d\n", sol.x[i], sol.B_L[sol.x[i]].size());
+     //printf("%d %d\n", sol.x[i], (int)sol.B_L[sol.x[i]].size());
+     fprintf(stderr, "%d %d\n", sol.x[i], (int)sol.B_L[sol.x[i]].size());
      for(int j = 0; j < sol.B_L[sol.x[i]].size(); j++)
      {
-       printf("%d ", sol.B_L[sol.x[i]][j]);
+	fprintf(stderr, "%d ", sol.B_L[sol.x[i]][j]);
+//       printf("%d ", sol.B_L[sol.x[i]][j]);
      }
-     printf("\n");
+	fprintf(stderr, "\n");
+//     printf("\n");
   }
 }
-
 struct x_solution iterated_local_search(struct x_solution best)
 {
-	return best;
-  int cont = 10;
+//	return best;
+  int cont = 50000;
   long f_best= eval(best);
 
   while(cont--)
   {
-	cout << cont <<endl;
     struct x_solution current = best;
+    int a = rand()%L;
+    int b = rand()%L;
+    iter_swap(current.x.begin()+a, current.x.begin()+b);
     random_shuffle(current.x.begin(), current.x.end());
     long f_current = eval(current);
     if( f_current > f_best)
     {
 	f_best= f_current;
 	best = current;
+	fprintf(stdout, "%ld %d\n", f_best, cont);
+        print_solution(best);
+	cont *=2;
     }
   }
  return best;
