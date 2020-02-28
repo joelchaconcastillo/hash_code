13f227edd6db0f31b321f526a01f56bdefc1487c
jcc <joel.chacon@cimat.mx>
2020-02-28 11:28:10 -0600

    ..

--- qualification/main.cpp
+++ qualification/main.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int B, L, D; //number of books, librearies and days....
-vector<int> scores_books, times_l, shipping_l;
+vector<int> score_books, signup_time_l, shipping_l; // score of each book, number of days it takes to finish the library to 
 vector<vector<int> > books_in_library;
 vector<int> counter_library;
 
@@ -11,58 +11,30 @@ struct x_solution
    vector<int> x; //indexes of the libraries...
    vector< vector<int> > B_L;
 };
+
 struct x_solution iterated_local_search(struct x_solution initial);
 void print_solution(struct x_solution &sol);
-long eval(struct x_solution &Sol)
-{
- long total_score = 0;
-  vector<int> cont_book_lib(L, 0);
-  vector<bool> checked(B, false);
-  int cont = 0;
-  int current_time = times_l[Sol.x[cont]];
-  for(int d = current_time; d <D; d++)
-  {
-     if(d >= current_time )
-       current_time +=times_l[Sol.x[++cont]];
-     for(int l = 0; l < cont; l++)//can be improved..
-     {
-	  int id_lib = Sol.x[l];
-	  for(int t = 0; t <  shipping_l[id_lib]; t++)//required..
-	  {
-            if( cont_book_lib[id_lib] < books_in_library[id_lib].size())
-            {
-            	      int id_book = books_in_library[id_lib][cont_book_lib[id_lib]++];
-            		 if( checked[id_book]) continue;
-                             total_score += scores_books[id_book];
-            		 checked[id_book]=true;
-            }
-	  }
-     }
-  }
- return total_score;
-}
+long eval(struct x_solution &Sol);
 
 int main()
 {
   struct x_solution sol;
   srand(1);
   scanf("%d %d %d\n", &B, &L, &D);
-  scores_books.resize(B);
+  score_books.resize(B);
   counter_library.resize(B, 0);
-  for(int i = 0; i < B; i++) scanf("%d ", &scores_books[i]);
+  for(int i = 0; i < B; i++) scanf("%d ", &score_books[i]);
   books_in_library.resize(L);
   sol.B_L.resize(L); 
   sol.x.resize(L);
 
-  times_l.resize(L);
+  signup_time_l.resize(L);
   shipping_l.resize(L);
   for(int i = 0; i < L; i++)
   { 
     int size_library;
-    scanf("%d %d %d\n", &size_library, &times_l[i], &shipping_l[i]); 
-    //cout << size_library<< " " << times_l[i] << " " <<shipping_l[i]<<endl;
+    scanf("%d %d %d\n", &size_library, &signup_time_l[i], &shipping_l[i]); 
     books_in_library[i].resize(size_library);
-//    sol.B_L[i].resize(size_library, 1); //all the book are active....
     for(int j = 0; j < books_in_library[i].size(); j++)
     {
        scanf("%d ", &books_in_library[i][j]);
@@ -124,4 +96,32 @@ struct x_solution iterated_local_search(struct x_solution best)
     }
   }
  return best;
+}
+long eval(struct x_solution &Sol)
+{
+ long total_score = 0;
+  vector<int> cont_book_lib(L, 0);
+  vector<bool> checked(B, false);
+  int cont = 0;
+  int current_time = signup_time_l[Sol.x[cont]];
+  for(int d = current_time; d <D; d++)
+  {
+     if(d >= current_time )
+       current_time +=signup_time_l[Sol.x[++cont]];
+     for(int l = 0; l < cont; l++)//can be improved..
+     {
+	  int id_lib = Sol.x[l];
+	  for(int t = 0; t <  shipping_l[id_lib]; t++)//required..
+	  {
+            if( cont_book_lib[id_lib] < books_in_library[id_lib].size())
+            {
+            	      int id_book = books_in_library[id_lib][cont_book_lib[id_lib]++];
+            		 if( checked[id_book]) continue;
+                             total_score += score_books[id_book];
+            		 checked[id_book]=true;
+            }
+	  }
+     }
+  }
+ return total_score;
 }
