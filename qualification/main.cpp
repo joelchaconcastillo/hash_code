7cc395a0eb92c9a30f07dc63ba37137fb11e7855
Joel <joel.chacon@cimat.mx>
2020-03-04 16:42:33 -0600

    ..

--- qualification/main.cpp
+++ qualification/main.cpp
@@ -9,12 +9,15 @@ vector<int> counter_books;
 ///the representation is by index of libraries and a mask indicating which book is active in each library..
 struct solution
 {
-   vector< pair< int, vector<int> > > x; // the first is library ids, second id of libraries..
+   vector<int> lib_order;
+   vector< vector<pair<int, bool> > > x; // the first is library ids, second id of libraries..
+   vector<vector<pair<int, int> > > idxBL; //indexes of books in libraries...
 };
 
 struct solution iterated_local_search(struct solution initial);
 void print_solution(struct solution &sol);
-long naive_eval(struct solution &Sol);
+//long naive_eval(struct solution &Sol);
+long fast_eval(struct solution &Sol);
 void sort_by_signup_time(struct solution &sol);
 void unique_books(struct solution &sol);
 solution hyper_heuristic();
@@ -25,32 +28,35 @@ int main()
   srand(1);
   load_data();
   solution sol = hyper_heuristic();
-
-//for(int i  = 0;i < B; i++) if(counter_books[i]) cout << counter_books[i]<< endl;
-//for(int i = 0; i <L; i++) cout << signup_time_l[i]<<endl;
-
-  //generating a random solution...
-//  for(int i = 0; i < L; i++)
-//    sol.x[i] = i; 
-//  random_shuffle(sol.x.begin(), sol.x.end());
-//  cout << naive_eval(sol)<<endl;
-//  print_solution(sol);
-//  struct solution improved = iterated_local_search(sol);
-//  printf("%ld\n\n", eval(improved));
-//  print_solution(improved);
   return 0;
 }
 void print_solution(struct solution &sol)
 {
+
+  vector<int> libs;
+  vector< vector<int> > l_b;
+  for(auto i = sol.lib_order.begin(); i != sol.lib_order.end(); i++)
+  {
+	vector<int> tmp;
+	for(auto j = sol.x[*i].begin(); j != sol.x[*i].end() ; j++)
+	   if( j->second) tmp.push_back(j->first);
+       if(!tmp.empty())
+       {
+	l_b.push_back(tmp);
+	libs.push_back(*i);
+       }
+  }
+
   //printf("%d\n", (int)sol.x.size());
-  fprintf(stderr, "%d\n", (int)sol.x.size());
-  for(int i = 0; i < (int)sol.x.size(); i++)
+  fprintf(stderr, "%d\n", (int)libs.size());
+  //for(auto i = sol.lib_order.begin(); i != sol.lib_order.end(); i++)
+  for(int i = 0; i < l_b.size(); i++)
   {
      //printf("%d %d\n", sol.x[i], (int)sol.B_L[sol.x[i]].size());
-     fprintf(stderr, "%d %d\n", sol.x[i].first, (int)sol.x[i].second.size());
-     for(int j = 0; j < sol.x[i].second.size(); j++)
+     fprintf(stderr, "%d %d\n", libs[i], (int)l_b[i].size());
+     for(int j = 0; j < l_b[i].size(); j++)
      {
-	fprintf(stderr, "%d ", sol.x[i].second[j]);
+	fprintf(stderr, "%d ", l_b[i][j]);
 //       printf("%d ", sol.B_L[sol.x[i]][j]);
      }
 	fprintf(stderr, "\n");
@@ -82,72 +88,54 @@ void print_solution(struct solution &sol)
 ///  }
 /// return best;
 ///}
-long naive_eval(struct solution &Sol)
-{
- long total_score = 0;
-  vector<int> cont_book_lib(L, 0);
-  vector<bool> checked(B, false);
-  int cont = 0;
-  int current_time = signup_time_l[Sol.x[cont].first];
-  for(int d = current_time; d <D; d++)
-  {
-     if(d >= current_time )
-       current_time +=signup_time_l[Sol.x[++cont].first];
-     for(int l = 0; l < cont; l++)//can be improved..
-     {
-	  int id_lib = Sol.x[l].first;
-	  for(int t = 0; t <  shipping_l[id_lib]; t++)//required..
-	  {
-            if( cont_book_lib[id_lib] < books_in_library[id_lib].size())
-            {
-//            	      int id_book = books_in_library[id_lib][cont_book_lib[id_lib]++];
-		      int id_book = Sol.x[l].second[cont_book_lib[id_lib]++];
-            		 if( checked[id_book]) continue;
-                             total_score += score_books[id_book];
-            		 checked[id_book]=true;
-            }
-	  }
-     }
-  }
- return total_score;
-}
-void unique_books(struct solution &sol)
-{
-   //for(int i = 0;i < sol.size(); i++)
-   //{
-   //}
-}
+//long naive_eval(struct solution &Sol)
+//{
+// long total_score = 0;
+//  vector<int> cont_book_lib(L, 0);
+//  vector<bool> checked(B, false);
+//  int cont = 0;
+//  int current_time = signup_time_l[Sol.x[cont].first];
+//  for(int d = current_time; d <D; d++)
+//  {
+//	cout <<cont <<endl;
+//     if(d >= current_time )
+//       current_time +=signup_time_l[Sol.x[++cont].first];
+//     for(int l = 0; l < cont; l++)//can be improved..
+//     {
+//	  int id_lib = Sol.x[l].first;
+//	  for(int t = 0; t <  shipping_l[id_lib]; t++)//required..
+//	  {
+//            if( cont_book_lib[id_lib] < Sol.x[l].second.size())
+//            {
+////            	      int id_book = books_in_library[id_lib][cont_book_lib[id_lib]++];
+//		      int id_book = Sol.x[l].second[cont_book_lib[id_lib]++];
+//            		 if( checked[id_book]) continue;
+//                             total_score += score_books[id_book];
+//            		 checked[id_book]=true;
+//            }
+//	  }
+//     }
+//  }
+// return total_score;
+//}
+
 
 void sort_by_signup_time(struct solution &S)
 {
-   solution prev = S;
    vector<pair<int, int> > vp;
    for(int i = 0; i < S.x.size(); i++)
-   vp.push_back(make_pair(signup_time_l[S.x[i].first], i));
+   vp.push_back(make_pair(signup_time_l[S.lib_order[i]], i));
    sort(vp.begin(), vp.end());
-   for(int i = 0; i < vp.size(); i++)
-     S.x[i] = prev.x[vp[i].second];
-//   priority_queue<pair<int, int>>pq;
-//   struct solution x_old = sol;
-//   for(int i = 0; i < L; i++)
-//   {
-//      pq.push(make_pair(-signup_time_l[i],i));
-//   }
-//   int cont = 0;
-//   while(!pq.empty())
-//   {
-//    sol.x[cont].second = x_old.x[pq.top().second].second;
-//    sol.x[cont++].first = pq.top().second;
-//    
-//    pq.pop();
-//   }
+   for(int i = 0; i < vp.size(); i++) S.lib_order[i] = vp[i].second;
 }
 solution hyper_heuristic()
 {
  solution S;
  initialization(S);
  sort_by_signup_time(S);
- cout << naive_eval(S);
+ unique_books(S);
+ print_solution(S);
+ cout << fast_eval(S);
  return S;
 }
 void load_data()
@@ -177,17 +165,66 @@ void load_data()
 }
 void initialization(solution &s)//initialization does not takes into consideration the repeated books.
 {
+   s.lib_order.resize(L);
    s.x.resize(L);
+   s.idxBL.resize(B);
    vector<bool> book_used(B, false);
    for(int i = 0; i <L ;i++)
    { 
-     s.x[i].first = i;
-     s.x[i].second.clear();
+     s.lib_order[i] = i;
+     s.x[i].clear();
      for(int j = 0; j < books_in_library[i].size(); j++)
      {
-	if( book_used[books_in_library[i][j]])continue;
-	s.x[i].second.push_back(books_in_library[i][j]);
+	s.idxBL[books_in_library[i][j]].push_back(make_pair(i,s.x[i].size()));
+	s.x[i].push_back(make_pair(books_in_library[i][j], !book_used[books_in_library[i][j]]));
 	book_used[books_in_library[i][j]] = true;
      }
    }
+}
+void unique_books(struct solution &s)
+{
+   vector<bool> u_books(B, false);
+   //for(int i = 0;i < s.x.size(); i++)
+   for(auto i = s.lib_order.begin(); i != s.lib_order.end() ; i++)
+   {
+      for(auto j = s.x[*i].begin();  j != s.x[*i].end(); j++)
+      {
+	  j->second = !u_books[j->first];
+          u_books[j->first] = true;
+      }
+   }
+}
+
+long fast_eval(struct solution &S)
+{
+ long total_score = 0;
+ vector< vector<int > > accum;
+ vector<int> lib;
+ for(auto i_lib = S.lib_order.begin(); i_lib != S.lib_order.end(); i_lib++)
+ {
+    int partial_score = 0;
+    vector<int> tmp;
+    for(auto i_book = S.x[*i_lib].begin(); i_book != S.x[*i_lib].end(); i_book++)
+    {
+	if( !i_book->second) continue;
+        partial_score += score_books[i_book->first]; 
+	tmp.push_back(partial_score);
+    }
+    if(tmp.empty()) continue;
+    accum.push_back(tmp);
+    lib.push_back(*i_lib);
+ }
+ for(int i = 0, day = 0; i < lib.size(); i++)
+ {
+    int id_lib = lib[i];
+    day += signup_time_l[id_lib];
+    if(day >=D) break;
+    int time_d;
+    if( (D-day) < (double)accum[i].size()/(double)shipping_l[id_lib]  ) //warning overflow.....
+      time_d =  (D - day)*shipping_l[id_lib];
+    else
+       time_d = accum[i].size();
+    total_score +=  accum[i][time_d-1];
+ }
+ return total_score;
 }
